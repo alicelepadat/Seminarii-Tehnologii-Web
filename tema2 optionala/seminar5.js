67e4ca06f7d7e04e97ded43b65addbbe5d431f0b
alicegeorgiana297 <alicegeorgiana297@gmail.com>
2020-10-31 17:21:57 +0200

    tema2 optionala

---
+++ tema2 optionala/seminar5.js
@@ -0,0 +1,126 @@
+/*Tema speciala 2: 
+a) Să se declare și să se folosească o variabilă ce va tine toate postarile utilizate in document. 
+    Api-ul de get se va apela o singura data, la început
+b) Să se implementeze mecanismul de edit, folosind principiile mecanismului de create. Asta presupune:
+    - Crearea unui api axios de PUT
+    - Crearea unui buton de edit, langa cel de delete
+    - Crearea unui form ce va primi și va edita informațiile obiectului curent
+    - Actualizarea informațiilor și reconstruirea tabelului*/
+
+const link = "https://jsonplaceholder.typicode.com/posts/"; 
+
+async function getPosts() {
+    try {
+        const posts = (await axios.get(link)).data;
+        return posts;
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+async function createPost(post) {
+    const response = (await axios.post(
+        link,
+        post,
+        {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+    )).data;
+    return response;
+}
+
+async function deletePosts(postId) {
+    const response = (await axios.delete(link + postId)).data;
+    return response;
+}
+
+
+function renderTable(posts) {
+    const existentTable = document.getElementById("renderTable");
+    if (existentTable) {
+        document.body.removeChild(existentTable);
+    }
+
+    var table = document.createElement("table");
+    var tableHeader = document.createElement("thead");
+    var tableBody = document.createElement("tbody");
+
+    var headerRow = document.createElement("tr");
+
+    for (prop in posts[0]) {
+        var headerCell = document.createElement("th");
+        headerCell.appendChild(document.createTextNode(prop));
+        headerRow.appendChild(headerCell);
+    }
+    tableHeader.appendChild(headerRow);
+
+    posts.forEach((post, index, self) => {
+        var bodyRow = document.createElement("tr");
+        for (prop in post) {
+            var bodyCell = document.createElement("td");
+            bodyCell.appendChild(document.createTextNode(post[prop]));
+            bodyRow.appendChild(bodyCell);
+        }
+    
+        var deleteButtonCell = document.createElement("td");
+        var deletebutton = document.createElement("button");
+        deletebutton.textContent = "Delete";
+
+        deletebutton.addEventListener("click", () => {
+            callDeletePost(post.id);
+            self.splice(index, 1);
+            renderTable(self);
+        })
+
+        deleteButtonCell.appendChild(deletebutton);
+        bodyRow.appendChild(deleteButtonCell);
+        tableBody.appendChild(bodyRow);
+    });
+
+    table.appendChild(tableHeader);
+    table.appendChild(tableBody);
+    table.id = "renderTable";
+    //table.style.cssText="border: 1px solid black; marginTop: 20px";
+
+    document.body.appendChild(table);
+}
+
+function addPostForm(event){
+    event.preventDefault();
+    const userId=document.getElementById("postUserID").value;
+    const title=document.getElementById("postTitle").value;
+    const body=document.getElementById("postBody").value;
+
+    if(!userId || !title || !body || userId <= 0) {
+        return;
+    }
+
+    callCreatePostwithInsert(
+        {id: 11, userId: userId, title: title, body: body}
+    );
+}
+
+function callGetPosts() {
+    getPosts().then(posts => renderTable(posts))
+        .catch(err => console.log(err));
+}
+
+function callCreatePostwithInsert(post) {
+    createPost(post).then(post => {
+        getPosts().then(posts=>{
+            posts.push(post);
+            renderTable(posts);
+        });
+    })
+}
+
+function callDeletePosts() {
+    deletePosts().then(resp => console.log(resp)).catch(err => console.log(err));
+}
+
+function callDeletePost(postId) {
+    deletePosts(postId).then(resp => console.log(resp)).catch(err => console.log(err));
+}
